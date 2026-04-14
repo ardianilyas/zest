815da2ef7ac1b334f5f7dadf9ca4dbaf1da990ad
ardianilyas <ardianilyas@gmail.com>
2026-04-14 17:37:54 +0700

    feat: update seeder add ticket seeder, clear data

--- src/seed/index.ts
+++ src/seed/index.ts
@@ -1,10 +1,23 @@
+import { db } from "../config/database";
+import { categories, ticketComments, tickets, users } from "../db/schema";
 import { categorySeed } from "./categories.seed";
+import { ticketSeed } from "./tickets.seed";
 import { userSeed } from "./user.seed";
 import "dotenv/config";
 
+async function clearDb() {
+  await db.delete(ticketComments);
+  await db.delete(tickets);
+  await db.delete(categories);
+  await db.delete(users);
+}
+
 async function main() {
-  await userSeed();
-  await categorySeed(20);
+  await clearDb();
+  
+  const users = await userSeed();
+  const categories = await categorySeed(20);
+  await ticketSeed(users, categories);
 }
 
 main().catch((err) => {
