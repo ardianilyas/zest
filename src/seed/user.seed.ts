14e31135a78846ca8ceb37603b82334ca1d261c4
ardianilyas <ardianilyas@gmail.com>
2026-04-14 17:35:20 +0700

    feat: add role enum seeder

--- src/seed/user.seed.ts
+++ src/seed/user.seed.ts
@@ -1,12 +1,13 @@
 import { faker } from '@faker-js/faker';
 import bcrypt from 'bcrypt';
 import { db } from '../config/database';
-import { users } from '../db/schema';
+import { userRoleEnum, users, type NewUser } from '../db/schema';
 
 export async function userSeed(total: number = 10) {
-  const data = await Promise.all(Array.from({ length: total }).map(async () => ({
+  const data: NewUser[] = await Promise.all(Array.from({ length: total }).map(async () => ({
     email: faker.internet.email(),
     password: await bcrypt.hash('password', 10),
+    role: faker.helpers.arrayElement(userRoleEnum.enumValues),
   })));
 
   const created = await db.insert(users).values(data).returning();
